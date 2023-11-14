e5363fdf1f5b03039ee64906ce5d99d1f3eaa6e4
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-11-14 17:16:10 +0100

    Add logging functionality with timestamp and stop command

--- logWriter.py
+++ logWriter.py
@@ -1,14 +1,19 @@
 from pynput import keyboard
 import time
+import os
+
 current_time = time.time()
 formatted_time = time.strftime("%H_%M_%d_%m", time.localtime(current_time))
 
+print("Press Alt Gr to stop logging")
+print("Logging will NOT be saved unless Alt Gr is pressed")
+
+
+os.makedirs('logs', exist_ok=True)
 file = open(f"logs/output_{formatted_time}.txt", "a")
 
 def get_time():
     return time.perf_counter_ns()
-
-nanoTimerStart = get_time()
 
 def type_correct(key): # Handles changing character standard
     try: 
@@ -17,13 +22,15 @@ def type_correct(key): # Handles changing character standard
         return listener.canonical(key) # If not make unmodified char
 
 def on_press(key):
-    file.write(f"Down {type_correct(key)} at {get_time() - nanoTimerStart}\n")
+    file.write(f"Down {type_correct(key)} at {get_time()}\n")
 
 def on_release(key): # Recieves as standard uncorrected key type
     if key == keyboard.Key.alt_gr:
         file.close() # Wont save unless closed
+        print("\n")
+        print("Logging stopped, window can be closed")
         return False # Stop listener
-    file.write(f"Up {type_correct(key)} at {get_time() - nanoTimerStart}\n")
+    file.write(f"Up {type_correct(key)} at {get_time()}\n")
 
 
 with keyboard.Listener(

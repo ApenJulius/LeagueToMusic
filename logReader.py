70c9fa1e500a0d39868be9a6929cc9edc0290156
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-11-14 13:42:10 +0100

    WIP removal of hold inputs

--- logReader.py
+++ logReader.py
@@ -29,12 +29,26 @@ def countUpKeys(fileLines):
     print(count, " up keys counted")
     return count
 
+def removeHoldLog(fileLines):
+    newFileLines = []
+    for downLine in fileLines:
+        downLineArray = downLine.split(" ")
+        if(downLineArray[0] != "Down"):
+            continue
+        for upLine in fileLines:
+            upLineArray = upLine.split(" ")
+            if upLineArray[0] == downLineArray[0] and upLineArray[1] == downLineArray[1]:
+                
+                break
+        if line.split(" ")[0] == "Up" and line.split(" ")[1] == "":
+            newFileLines.append(line)
 
+    print(newFileLines)
 
 
 
 if __name__ == __name__:
-    with open("logs/output_11_29_14_11.txt") as logFile:
+    with open("logs/output_12_54_14_11.txt") as logFile:
         logLines = logFile.readlines()
         findAllUniqueKeys(logLines)
-        countUpKeys(logLines)
+        removeHoldLog(logLines)
